6c234b72420d6f745b421adbb44b91ea542cc5bf
Deniz Gürsoy <denizgursoy.443@gmail.com>
2021-03-31 21:54:37 +0300

    scanning of question and exam was added

--- repos/implementation/dynamoDb-ExamRepo.go
+++ repos/implementation/dynamoDb-ExamRepo.go
@@ -1,10 +1,13 @@
 package implementation
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 	"github.com/google/uuid"
 	"github.com/rabatiproject/testing-application-backend/model/base"
+	"log"
 )
 
 type dynamoDbExamRepo struct {
@@ -31,7 +34,32 @@ func (d *dynamoDbExamRepo) SaveExam(exam *base.Exam) error {
 }
 
 func (d *dynamoDbExamRepo) ExamExists(examId string) bool {
-	panic("implement me")
+	dynamodbClient := getNewClient()
+
+	filt := expression.Name("id").Equal(expression.Value(examId))
+
+	proj := expression.NamesList(expression.Name("id"))
+
+	expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
+	if err != nil {
+		log.Fatalf("Got error building expression: %s", err)
+	}
+
+	params := &dynamodb.ScanInput{
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		FilterExpression:          expr.Filter(),
+		ProjectionExpression:      expr.Projection(),
+		TableName:                 aws.String(NewDynamoDbRepo().TableName),
+	}
+
+	// Make the DynamoDB Query API call
+	result, err := dynamodbClient.Scan(params)
+
+	if err != nil {
+		log.Fatalf("Query API call failed: %s", err)
+	}
+	return len(result.Items) > 0
 }
 
 func (d *dynamoDbExamRepo) ExamContainsQuestions(questionId string) bool {
